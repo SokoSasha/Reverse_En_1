ed1f94c98e6c61ce0562d90c3126f886174d26c7
Danil <frolovd@vtb.ru>
2022-11-03 19:58:16 +0300

    Added useless "password" checks and some garbage

--- Victim.cpp
+++ Victim.cpp
@@ -3,18 +3,24 @@
 #include <string>
 using namespace std;
 
+//A class of hidden string to display
 class ObString {
 public:
 
-#define InitMes Messages.Init()
-#define PathS Messages.Path()
-#define ShutMes Messages.Shut()
-#define MenuMes Messages.Menu()
-#define BackMes Messages.Back()
-#define TryMes Messages.Try()
-#define WrongMes Messages.Wrong()
-#define PW Messages.PassWord()
-#define OFF (qwerty == Messages.OF())
+#define InitMes Secret.Init()
+#define PathS Secret.Path()
+#define ShutMes Secret.Shut()
+#define MenuMes Secret.Menu()
+#define BackMes Secret.Back()
+#define TryMes Secret.Try()
+#define WrongMes Secret.Wrong()
+#define PW Secret.PassWord()
+#define OFF (qwerty == Secret.OF())
+#define POZ Secret.Poz()
+#define FP1 Secret.FP(1)
+#define FP2 Secret.FP(2)
+#define FP3 Secret.FP(3)
+#define FP4 Secret.FP(4)
 
 	void Init() {
 		cout << Decode(init_str);
@@ -52,6 +58,30 @@ public:
 		return Decode(off);
 	}
 
+	const char* Poz() {
+		return Decode(poz);
+	}
+
+	const string FP(int i) {
+		switch (i)
+		{
+		case 1: {
+			return Decode(fp1);
+		}
+		case 2: {
+			return Decode(fp2);
+		}
+		case 3: {
+			return Decode(fp3);
+		}
+		case 4: {
+			return Decode(fp4);
+		}
+		default:
+			break;
+		}
+	}
+
 	void Encode(const string str) {
 		string enc;
 		enc.resize(str.size());
@@ -70,6 +100,15 @@ private:
 		}
 		return back;
 	}
+
+	const char* Decode(const char* dec) {
+		char* back = new char[6];
+		for (int i = 0; i <5; i++)
+			back[i] = dec[i] - i;
+		back[5] = '\0';
+		return back;
+	}
+
 	const string init_str = "Pmgdwj2'mw~p~-~pƒ„‰‚†y6‹‡9Š‹€ƒ„„+`aD";
 	const string menu = "[2_#Wmu~(os}.x€€x4ƒ‹„z~Œ%wO{?s‰‘šD†’“H™’šš’N¥ž”˜¦¨@’h–Z€´¦²I~b";
 	const string path = "C;^Vexnhd<*ÕüþjD0â÷ÿù\x6\b\atëêãëëzkƒƒT€™‹ŸœWž£ ";
@@ -79,9 +118,15 @@ private:
 	const string wrong = "Wsqqk%vh{|z~q/";
 	const string password = "14;9<FF";
 	const string off = "0123";
+	const char* poz = "pbwvi";
+	const string fp1 = "1234";
+	const string fp2 = "aaaa";
+	const string fp3 = "qwertyuiop";
+	const string fp4 = "password";
 };
 
-ObString Messages;
+//Hidden messages
+ObString Secret;
 
 const string oneNum(void) {
 	string number;
@@ -113,6 +158,24 @@ bool checkPass(string qwer) {
 	return qwer == PW;
 }
 
+//Some garbage to distract "tHe HaCkErS"
+void VeryUsefullFunc() {
+	char haha = '!';
+	while (0) {
+		haha++;
+	}
+	if (haha != haha) {
+		int gar = 12898123;
+		gar--;
+		gar *= 2;
+		gar = gar << 4;
+	}
+	float trhn = 792.6163;
+	while (trhn != 0) {
+		trhn /= 3;
+	}
+}
+
 int main() {
 	while (1) {
 		InitMes;
@@ -120,14 +183,22 @@ int main() {
 		cin >> qwerty;
 		if (OFF) {
 			ShutMes;
-			system("pause");
+			system(POZ);
 			return 0;
 		}
+		//////////False password check////////////
+		if (qwerty == FP1 || qwerty == FP2)
+			VeryUsefullFunc();
+		/////////////////////////////////////////
 		if (checkPass(qwerty)) {
 			int ch = 1;
 			while (ch) {
 				bool out = 0;
 				MenuMes;
+				////False password check////
+				if (qwerty == FP3)
+					VeryUsefullFunc();
+				////////////////////////////
 				cin >> ch;
 				switch (ch)
 				{
@@ -138,15 +209,35 @@ int main() {
 					break;
 				}
 				case 1: {
-					cout << oneNum() << endl;
+					if (checkPass(qwerty))
+						cout << oneNum() << endl;
+					else {
+						out = 1;
+						ch = 1;
+					}
 					break;
 				}
 				case 2: {
-					cout << allNums() << endl;
+					if (checkPass(qwerty))
+						cout << allNums() << endl;
+					else {
+						//////False password check/////
+						if (qwerty == FP4)
+							VeryUsefullFunc();
+						///////////////////////////////
+
+						out = 1;
+						ch = 1;
+					}
 					break;
 				}
 				default: {
-					TryMes;
+					if (checkPass(qwerty))
+						TryMes;
+					else {
+						out = 1;
+						ch = 1;
+					}
 					break;
 				}
 				}
@@ -156,7 +247,7 @@ int main() {
 		else WrongMes;
 	}
 
-	system("pause");
+	system(POZ);
 	return 0;
 	
 }
